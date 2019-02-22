b378f6b36572a5fac63e1fcc7723a125241e7f12
elie-elia <elie.elia@mail.mcgill.ca>
2019-02-21 19:05:58 -0500

    Updateblock

--- Iteration3/src/ca/mcgill/ecse223/block/application/Block223Application.java
+++ Iteration3/src/ca/mcgill/ecse223/block/application/Block223Application.java
@@ -1,4 +1,5 @@
 package ca.mcgill.ecse223.block.application;
+import ca.mcgill.ecse223.block.model.Game;
 
 import ca.mcgill.ecse223.block.model.Block223;
 import ca.mcgill.ecse223.block.model.Game;
@@ -7,6 +8,7 @@ import ca.mcgill.ecse223.block.model.Level;
 public class Block223Application {
 	
 	private static Block223 block223; 
+	private static Game game;
 	
 	public static Block223 getBlock223() {
 		//temporary implementation
@@ -19,8 +21,8 @@ public class Block223Application {
 	}
 
 	public static Game getCurrentGame() {
-		// temporary
-		return null; 
+//to be implemented
+		return null;
 	}
 
 	public static void getLevel(Level level) {
