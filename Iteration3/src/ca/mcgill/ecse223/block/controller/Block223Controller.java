2cc42ffc60c3a77fbcd92f33f9c430d9553cfa2d
elie-elia <elie.elia@mail.mcgill.ca>
2019-03-04 18:12:46 -0500

    Update Block223Controller.java

--- Iteration3/src/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ Iteration3/src/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -356,6 +356,16 @@ public class Block223Controller {
         	throw new RuntimeException(e.getMessage());
         	}
 }
+		
+		if (gridVerticalPosition !=0 && gridVerticalPosition > 0 && gridVerticalPosition <= maxNumberOfVerticalBlocks) {
+			try {
+		BlockAssignment blockassignment = new BlockAssignment(gridHorizontalPosition, gridVerticalPosition,
+				currentlevel, block, game);
+			}
+		catch(RuntimeException e) {
+        	throw new RuntimeException(e.getMessage());
+        	}
+}
 
 	}
 	
